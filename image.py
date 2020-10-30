66dde28ba2b94edbc25db61e535dab3f60fef17e
Timedropper <2697446335@qq.com>
2020-10-30 21:14:21 +0800

    图片转字符画

--- image.py
+++ image.py
@@ -1,24 +1,17 @@
-#!/data/data/com.termux/files/usr/bin/python
-import numpy as np
+#!/usr/bin/env python
 from sys import argv
-from PIL import Image
-string = [
-        '[]', '{}', '€€', '#1', '%%', '77', '**', '==',
-        '++', '  ', '<>', '¥%', '??', '()', '::',r'\/'
+from numpy import array
+from PIL.Image import open as img_open
+str_line = [
+        '[)', '<}', '€¥', '#1', '%V', 'l7', '**', '+=',
+        '-+', '  ', '{>', '|%', '??', '(]', '::',r'\/'
         ]
-if len(argv) < 3:
-    print(f'格式: {argv[0]} <图片> <文本>')
-    exit()
-img = Image.open(argv[1])
-img = img.convert('L')
-img.thumbnail((70,img.size[1]))
-arr = np.array(img)
-arr = ((arr+1)/16).tolist()
-length = range(len(arr))
-for i1 in length:
-    for i2 in range(len(arr[i1])):
-        arr[i1][i2] = string[int('{:.0f}'.format(arr[i1][i2]))-1]
-for i in length:
-    arr[i] = ''.join(arr[i])+'\n'
-with open(argv[2],'w') as f:
-    f.writelines(arr)
+def img_to_text(image,text):
+    img = img_open(image)
+    img = img.resize((190,88))
+    img = img.convert('L')
+    img = (array(img)//16).tolist()
+    char = [''.join([str_line[i2] for i2 in i1]) + '\n' for i1 in img]
+    with open(text,'w') as txt:
+        txt.writelines(char)
+img_to_text(argv[1],argv[2])
